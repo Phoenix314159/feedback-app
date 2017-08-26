5f0f3a16a888b0abda7c2f66a5738a1ce79a9e32
James Langford <derivative7117@live.com>
2017-08-26 14:04:22 -0600

    updated prod.js

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
     cookieAge: 2592000000,
-    redirectUrl: `https://feedback-app123.herokuapp.com/`
+    redirectUrl: `https://feedback-app-12345.herokuapp.com/`
 }
