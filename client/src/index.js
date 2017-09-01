2503a9ad41b93caf9de9f4b8d5ca8e8fe576d522
James Langford <derivative7117@live.com>
2017-09-01 13:53:52 -0600

    added payment component

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-const stripePubishableKey = 'pk_test_YCIPURTU6ePqrjERaHH1AHMN'
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
