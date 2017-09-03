0cc947976472912a0122c30c0159f05060617769
James Langford <derivative7117@live.com>
2017-09-03 13:21:55 -0600

    udtaed pro,js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT,
-    db: process.env.MONGO_URI,
+    mongoURI: process.env.MONGO_URI,
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
