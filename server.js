5e75fd087a05ed1bcd01e70a1e4aebf5a33ad24c
James Langford <derivative7117@live.com>
2017-09-04 14:17:38 -0600

    modified gitignore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const config = require('./config/config'),
     express = require('express'),
     mongoose = require('mongoose'),
+    path = require('path'),
     port = process.env.PORT || 3367,
     app = express();
 
@@ -13,13 +14,15 @@ require('./routes/auth')(app);
 require('./routes/billingRoutes')(app);
 require('./routes/getAlbums')(app);
 
-//<----------- production --------------->
-// process.env.PWD = process.cwd();
-// app.use(express.static(process.env.PWD + '/client/build'));
-// app.get('*', (req, res) => {
-//     res.sendFile(process.env.PWD  + '/client/build/index.html');
-// })
-//<-------------------------------------->
+//<---------- production ----------->
+if (process.env.NODE_ENV === 'production') {
+    process.env.PWD = process.cwd();
+    app.use(express.static(process.env.PWD + '/client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(process.env.PWD, 'client', 'build', 'index.html'));
+    })
+}
+//<--------------------------------->
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
