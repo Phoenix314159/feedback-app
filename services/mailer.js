c78585f4fdec623c0645caeebe774f53cb47abfc
James Langford <derivative7117@live.com>
2017-09-10 15:17:44 -0600

    send e-mails

--- services/mailer.js
+++ services/mailer.js
@@ -3,12 +3,55 @@ const sendgrid = require('sendgrid'),
     config = require('../config/config');
 
 class Mailer extends helper.Mail {
-    constructor({ subject, recipients}, content) {
-      super();
-      this.from_email = new helper.Email('no-reply@feedbackapp.com');
-      this.subject = subject;
-      this.body = new helper.Content('text/html', content);
-      this.recipients = this.formatAddresses(recipients);
+    constructor({subject, recipients}, content) {
+        super();
+        this.sgApi = sendgrid(config.sendGridKey);
+        this.from_email = new helper.Email('no-reply@feedbackapp.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({email}) => {
+            return new helper.Email(email);
+        })
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings(),
+            clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        })
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+        const response = await this.sgApi.API(request, (error, response) => {
+            if (error) {
+                console.log("Error response received");
+            }
+            console.log('status code: ', response.statusCode);
+            console.log('body: ', response.body);
+            console.log('headers: ', response.headers);
+        });
+        return response;
     }
 }
 
