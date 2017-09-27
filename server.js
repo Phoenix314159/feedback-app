d1fae42933070baca2c895d3832f25f1e68a1a28
James Langford <derivative7117@live.com>
2017-09-27 16:47:17 -0600

    deplot to heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const config = require('./config/config'),
-    app = require('express')(),
+    express = require('express'),
     mongoose = require('mongoose'),
-    path = require('path');
+    path = require('path'),
+    app = express();
 
 mongoose.connect(config.mongoURI);
 
@@ -14,13 +15,13 @@ require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
 //<---------- production ----------->
-if (process.env.NODE_ENV === 'production') {
+
     process.env.PWD = process.cwd();
     app.use(express.static(process.env.PWD + '/client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(process.env.PWD, 'client', 'build', 'index.html'));
     })
-}
+
 //<--------------------------------->
 
 app.listen(config.port, () => {
